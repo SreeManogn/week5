61efb0d5d240d72aaa4986b90dde3bb106ea4b1d
Sree Manogna <22251a1257it@gmail.com>
2025-08-12 09:16:48 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,14 +1,15 @@
 public class Factorial{
   public static long compute(int n){
     if(n<0) throw new IllegalArgumentException("Negative number not allowed");
-    long result=1;
+    long res=1;
     for(int i=2;i<=n;i++){
-      result*=i;
+      res*=i;
     }
-    return result;
+    return res;
   }
   public static void main(String args[]){
   int n=5;
     System.out.println("Factorial of"+ n +"is"+compute(n));
   }
 }
+
